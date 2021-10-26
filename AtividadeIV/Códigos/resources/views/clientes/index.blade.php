86e6a866c7b50c2e755ddbaf3824f73023398451
John Junqueira <81057973+JohnProfissional@users.noreply.github.com>
2021-10-26 13:39:36 -0300

    Add files via upload

---
+++ AtividadeIV/Códigos/resources/views/clientes/index.blade.php
@@ -0,0 +1,10 @@
+<h1>Página de clientes</h1>
+
+
+@foreach ($clientes as $cliente)
+<ul>
+    <li>Nome do cliente: {{$cliente->nome}}</li>
+    <li>Endereco do cliente: {{$cliente->endereco}}</li>
+    <li>debito do cliente: {{$cliente->debito}}</li>
+</ul>
+@endforeach 
