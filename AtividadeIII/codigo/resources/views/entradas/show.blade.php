75ac371ae2bda6e82d58d0d53c207d69fdd7224c
John Junqueira <81057973+JohnProfissional@users.noreply.github.com>
2021-11-07 12:49:03 -0300

    Delete AtividadeIII/codigo/resources/views/entradas directory

--- AtividadeIII/codigo/resources/views/entradas/show.blade.php
+++
@@ -1,9 +0,0 @@
-<h1>Página de consulta de entradas</h1>
-
-@foreach ($entradas as $entrada)
-<ul>
-    <li>Valor total da nota: {{$entrada->valortotaldanota}}</li>
-    <li>Data da entrada: {{$entrada->dataentrada}}</li>
-    
-</ul>
-@endforeach 
