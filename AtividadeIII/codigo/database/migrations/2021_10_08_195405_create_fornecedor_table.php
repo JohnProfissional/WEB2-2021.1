6b440c425e36e4ac31eb0af1ce61ebbba3ed31fe
John Junqueira <81057973+JohnProfissional@users.noreply.github.com>
2021-11-07 12:41:12 -0300

    Add files via upload

---
+++ AtividadeIII/codigo/database/migrations/2021_10_08_195405_create_fornecedor_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFornecedorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fornecedor', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nome', 60);
+            $table->string('endereco', 60);
+            $table->string('email', 60);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fornecedor');
+    }
+}
---
+++ AtividadeIII/codigo/database/migrations/2021_11_02_022723_create_entrada_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntradaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entrada', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->double('valortotaldanota', 7,2);
+            $table->date('dataentrada');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entrada');
+    }
+}
